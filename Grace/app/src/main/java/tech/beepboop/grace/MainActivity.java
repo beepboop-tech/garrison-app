95d6b085f0c3220a0ddf2c75b958849015ce7bb8
Tom Evans <TomosAMEvans@gmail.com>
2018-01-16 17:50:51 +0000

    bl
    kl

--- Grace/app/src/main/java/tech/beepboop/grace/MainActivity.java
+++ Grace/app/src/main/java/tech/beepboop/grace/MainActivity.java
@@ -18,10 +18,10 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText(R.string.title_menu);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    mTextMessage.setText(R.string.title_add);
                     return true;
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
